2d000a82e31e04178e06ae92f0e8efcc4f13534c
Emil Stenström <em@kth.se>
2019-01-11 14:34:05 +0100

    Make production mode the default.

--- server.py
+++ server.py
@@ -6,14 +6,11 @@ import os
 
 if __name__ == "__main__":
     environment = os.environ.get("ENVIRONMENT", None)
-    assert environment, "Needs $ENVIRONMENT variable set"
     if environment == "development":
         print("RUNNING IN DEVELOPMENT MODE")
         bottle.debug(True)
         bottle.TEMPLATES.clear()
         run(host='localhost', port=9000, reloader=True)
-    elif environment == "production":
+    else:
         print("RUNNING IN PRODUCTION MODE")
         run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-    else:
-        assert False, "That's not a valid $ENVIRONMENT"
