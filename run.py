d7e06361eea7ea38b6fe37f318c4b6377d66c9c0
Emil Stenström <emil@emilstenstrom.se>
2021-03-22 10:47:13 +0100

    Update run.py to latest version.

--- run.py
+++ run.py
@@ -1,14 +1,36 @@
 #!/usr/bin/env python
-import os
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('--run', action="store_true")
-parser.add_argument('--deploy', action="store_true")
-args = parser.parse_args()
+import os
 
-if not any(vars(args).values()):
-    parser.print_help()
-elif args.run:
-    os.system("ENVIRONMENT=development python server.py")
-elif args.deploy:
-    os.system("git push heroku master")
+def run():
+    """ Reuse the Procfile to start the dev server """
+    with open("Procfile", "r") as f:
+        command = f.read().strip()
+
+    command = command.replace("web: ", "")
+    os.system("ENVIRONMENT=development " + command)
+
+def deploy():
+    os.system("git push dokku master")
+
+def dependencies():
+    os.system("pip-compile --upgrade requirements.in")
+    os.system("pip-compile --upgrade requirements-dev.in")
+    os.system("pip-sync requirements-dev.txt")
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--deploy', action="store_true", required=False)
+    parser.add_argument('--deps', action="store_true", required=False)
+    args = parser.parse_args()
+
+    if args.deploy:
+        deploy()
+    elif args.deps:
+        dependencies()
+    else:
+        run()
+
+
+if __name__ == '__main__':
+    main()
